aa2ae12ef91c3f740ae8acaefcd5f739229f7feb
Ashkan Kamyab <ashkankamyab@gmail.com>
2018-09-09 14:09:29 +0200

    OS Detection is fixed

--- huebsh.sh
+++ huebsh.sh
@@ -1,26 +1,43 @@
 #/bin/bash
-
-KNOWN_DISTRIBUTION="(Debian|Ubuntu|RedHat|CentOS|openSUSE|SUSE)"
+KNOWN_DISTRIBUTION="(Debian|Ubuntu|RedHat|CentOS|openSUSE|Amazon|Arista|SUSE)"
 DISTRIBUTION=$(lsb_release -d 2>/dev/null | grep -Eo $KNOWN_DISTRIBUTION  || grep -Eo $KNOWN_DISTRIBUTION /etc/issue 2>/dev/null || grep -Eo $KNOWN_DISTRIBUTION /etc/Eos-release 2>/dev/null || grep -m1 -Eo $KNOWN_DISTRIBUTION /etc/os-release 2>/dev/null || uname -s)
 
 if [ $DISTRIBUTION = "Darwin" ]; then
-    printf "\033[31mThis script does not support installing on the Mac."
+    printf "\033[31mThis script does not support installing on the Mac.
+
+Please use the 1-step script available at https://app.datadoghq.com/account/settings#agent/mac.\033[0m\n"
     exit 1;
 
 elif [ -f /etc/debian_version -o "$DISTRIBUTION" == "Debian" -o "$DISTRIBUTION" == "Ubuntu" ]; then
     OS="Debian"
+    echo $OS
 elif [ -f /etc/redhat-release -o "$DISTRIBUTION" == "RedHat" -o "$DISTRIBUTION" == "CentOS" -o "$DISTRIBUTION" == "Amazon" ]; then
     OS="RedHat"
+    echo $OS
+# Some newer distros like Amazon may not have a redhat-release file
+elif [ -f /etc/system-release -o "$DISTRIBUTION" == "Amazon" ]; then
+    OS="RedHat"
+    echo $OS
+# Arista is based off of Fedora14/18 but do not have /etc/redhat-release
+elif [ -f /etc/Eos-release -o "$DISTRIBUTION" == "Arista" ]; then
+    OS="RedHat"
+    echo $OS
 # openSUSE and SUSE use /etc/SuSE-release or /etc/os-release
 elif [ -f /etc/SuSE-release -o "$DISTRIBUTION" == "SUSE" -o "$DISTRIBUTION" == "openSUSE" ]; then
     OS="SUSE"
-    exit
+    echo $OS
 fi
 
-if [ $OS="RedHat"] ; then
-    echo -e "Installing $OS Base necessary applications"
-    yum -y update && yum -y install $(cat requirements.txt)
-
+# Root user detection
+if [ $(echo "$UID") = "0" ]; then
+    sudo_cmd=''
 else
-    echo -e "Installing $OS Bae necessary applications"
-    apt install -y requirements.txt
+    sudo_cmd='sudo'
+fi
+#if [ $OS="RedHat"] ; then
+#    echo -e "Installing $OS Base necessary applications"
+#    yum -y update && yum -y install $(cat requirements.txt)
+
+#else
+#    echo -e "Installing $OS Bae necessary applications"
+#    apt install -y requirements.txt
